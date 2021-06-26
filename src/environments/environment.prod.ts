2548a9792c48e9cae284f0594b95a7b64e8079f5
Carlos Lobo <carloslobo@wposs.com>
2021-06-26 11:15:59 -0500

    Se realiza build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBo5gP_MtA3vgGZIy9ZSs_cT8SXOaTA4a8',
+    authDomain: 'firechat-4f569.firebaseapp.com',
+    projectId: 'firechat-4f569',
+    storageBucket: 'firechat-4f569.appspot.com',
+    messagingSenderId: '179921629406',
+    appId: '1:179921629406:web:eb41b59dcc7767e6bf524f',
+    measurementId: 'G-Q6XBMPMT5Q',
+  },
 };
